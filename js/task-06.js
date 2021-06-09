465db7997505caf3efd7406e87c8cc5353fcfba1
NikGorbunov <72225015+NikGorbunov@users.noreply.github.com>
2021-06-09 10:14:31 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,15 @@
-const inputEl = document.querySelector('input');
+const inputEl = document.querySelector("input");
 
-inputEl.addEventListener('focus', contentCheck);
+inputEl.addEventListener("focus", contentCheck);
 
 function contentCheck(textInInput) {
-  // if (params.currentTarget.value.length > 6) {
-  //   console.log(inputEl.classList.add('invalid'));
-  // } else {
-  //   inputEl.classList.add('valid');
-  // }
+  if (textInInput.currentTarget.value.length >= 6) {
+    console.log(inputEl.classList.add("invalid"));
+  } else {
+    inputEl.classList.add("valid");
+  }
 
-  textInInput.currentTarget.value.length >= 6
-    ? inputEl.classList.add('invalid')
-    : inputEl.classList.add('valid');
+  // textInInput.currentTarget.value.length >= 6
+  //   ? inputEl.classList.add('invalid')
+  //   : inputEl.classList.add('valid');
 }

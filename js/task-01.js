67ba30e3b58990e84a711483b08513958eb3d54e
NikGorbunov <72225015+NikGorbunov@users.noreply.github.com>
2021-06-13 16:59:43 +0300

    hw7

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,9 @@
-const categoriesEl = document.querySelectorAll('ul#categories li.item');
+const categoriesEl = document.querySelectorAll("ul#categories li.item");
 const countCategoriesEl = categoriesEl.length;
 console.log(`В списке ${countCategoriesEl} категории`);
 
-categoriesEl.forEach(category => {
-  return (
-    console.log('Категория: ', category.querySelector('h2').textContent),
-    console.log(
-      'Количество элементов: ',
-      category.querySelectorAll('li').length
-    )
-  );
-});
+categoriesEl.forEach(
+  (category) =>
+    console.log("Категория: ", category.querySelector("h2").textContent),
+  console.log("Количество элементов: ", category.querySelectorAll("li").length)
+);

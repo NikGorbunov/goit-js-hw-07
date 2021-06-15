6f746263cd0f8cd7eb8597893d9b7867b19b78b1
NikGorbunov <72225015+NikGorbunov@users.noreply.github.com>
2021-06-15 14:58:44 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -6,13 +6,22 @@ const decrementButtonEl = document.querySelector(
   '#counter button[data-action="decrement"]'
 );
 
+let counterValue = 0;
+const onIncrementBtnClick = () => {
+  counterValue += 1;
+
+  counterValueEl.textContent = counterValue;
+};
+
+const onDecrementBtnClick = () => {
+  counterValue -= 1;
+
+  counterValueEl.textContent = counterValue;
+};
+
 decrementButtonEl.addEventListener("click", onDecrementBtnClick);
 incrementButtonEl.addEventListener("click", onIncrementBtnClick);
 
-function onDecrementBtnClick() {
-  counterValueEl.textContent--;
-}
-
-function onIncrementBtnClick() {
-  counterValueEl.textContent++;
-}
+// "Заведите отдельную переменную (число),
+// значение которой увеличивается / уменьшается на 1(при клике на соответсвующую кнопку),
+//   и записывайте это значение в textContent спана с id = "value"."
